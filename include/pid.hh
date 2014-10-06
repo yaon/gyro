671daacb06d8aa160abb59951288d22d5a6c2a83
Willer <ly.william.chhim@gmail.com>
2014-10-06 10:02:31 -0400

    Implement the PID algorithm

--- include/pid.hh
+++ include/pid.hh
@@ -15,6 +15,17 @@ class PID
   public:
     PID();
     // return the new motor speed depending on the position of accelerometer
+    // the value is between -128 and 127
     int operator()(int value);
   private:
+    // Coefficient used for the PID
+    const float kp;
+    const float ki;
+    const float kd;
+
+    // error_sum is used to calculate the integrate term
+    int error_sum;
+
+    // last_error is used to calculate the derivative term
+    int last_error;
 };
--- src/pid.cc
+++ src/pid.cc
@@ -8,23 +8,58 @@
    2015
 */
 #include "pid.hh"
+#include <iostream>
 
-PID::PID() {}
+PID::PID()
+  : kp(1), ki(1), kd(1), last_error(0), error_sum(0)
+{}
 
 int PID::operator()(int value)
 {
-  if (value > 3996)
-    return -30;
-  else if (value < 100)
-    return 30;
-  else if (value > 3796)
-    return -40;
-  else if (value < 300)
-    return 40;
-  else if (value < 1024)
-    return 50;
-  else if (value > 2096)
-    return -50;
-  else
-    return 0;
+  // values of the pid
+  int p_val = 0;
+  int i_val = 0;
+  int d_val = 0;
+
+  // value is between 0 and 1024 whenever the gyropod is tilted forward
+  // and between 4096 - 1024 and 4096 if it is tilted backward
+  int error = value < 1024 ? value : value - 4096;
+
+  // proportional term
+  p_val = error * kp;
+
+  // integrate term
+  error_sum += error;
+  i_val = error_sum * ki;
+
+  // derivative term
+  d_val = last_error * kd;
+
+  int pid = p_val + i_val + d_val;
+
+  // update the last error for the next iteration
+  last_error = error;
+
+  // control the max value of the pid
+  if (pid > 1023)
+    pid = 1023;
+  else if (pid < -1024)
+    pid = -1024;
+
+  // set the value between -128 and 127 for the motor
+  pid = (pid * 128) / 1024;
+
+  // debug check
+  if (pid > 127)
+  {
+    std::cerr << "[D]: Error on the pid value (> 127) : " << pid << std::endl;
+    pid = 127;
+  }
+  else if (pid < -128)
+  {
+    std::cerr << "[D]: Error on the pid value (< -128) : " << pid << std::endl;
+    pid = -128;
+  }
+
+  return pid;
 }
